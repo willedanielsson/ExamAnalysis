b46f0f78bd551f82976221f1bbf9a25a4151e24b
William Danielsson <William@williams-mbp.lan>
2015-10-30 15:33:52 +0100

    Local server running and added ignore

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+var http = require('http');
+
+const PORT = 8080;
+
+function handleRequest(request, response){
+	response.end('It works! Path hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+	console.log("Server listening on: http://localhost:%s", PORT);
+});
